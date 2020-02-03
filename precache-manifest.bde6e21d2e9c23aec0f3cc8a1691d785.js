5041fe1c90a0859a5a93807b4a03f7d095c564fe
Sambo <losangeles9@gmail.com>
2020-02-03 23:42:45 +0100

    :rocket:

---
+++ precache-manifest.bde6e21d2e9c23aec0f3cc8a1691d785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8932b7013c16464fd5a9f89f41499b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b079ce4db9c1dcc18280",
+    "url": "/static/css/main.aeded17c.chunk.css"
+  },
+  {
+    "revision": "96e49a662c4d3f6eebb0",
+    "url": "/static/js/2.ecf47973.chunk.js"
+  },
+  {
+    "revision": "0dec0ffb2821969b0e8da48d68f6c870",
+    "url": "/static/js/2.ecf47973.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b079ce4db9c1dcc18280",
+    "url": "/static/js/main.e1705626.chunk.js"
+  },
+  {
+    "revision": "03af0edb624deb2c762d",
+    "url": "/static/js/runtime-main.583a9879.js"
+  }
+]);
